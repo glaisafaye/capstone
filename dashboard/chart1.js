3224104f0c81ab3cc4fb5dacc463c16764464fb1
Glaisa Faye Adora <glaisafayeadora@gmail.com>
2023-09-12 17:02:48 +0800

    sdf

--- dashboard/chart1.js
+++ dashboard/chart1.js
@@ -1,25 +1,39 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const ctx = document.getElementById('lineChart');
+  const ctx = document.getElementById('lineChart'); // Change 'lineChart' to 'barChart'
 
   // Fetch data from the PHP script
-    fetch('/mis/dashboard/fetch_civil_status_data.php')
+  fetch('/mis/dashboard/fetch_population.php') // Correct the path to the PHP script
     .then(response => response.json())
     .then(data => {
+      const population2020 = data.totalPopulation;
+      const population2015 = 1486; // Use the total population value for 2015
+
+      const percentageDifference = ((population2020 - population2015) / population2015) * 100;
+      let changeDescription = '';
+
+      if (percentageDifference > 0) {
+        changeDescription = 'increase';
+      } else if (percentageDifference < 0) {
+        changeDescription = 'decrease';
+      } else {
+        changeDescription = 'stay the same';
+      }
+
       new Chart(ctx, {
-        type: 'line',  // Corrected chart type
+        type: 'bar', // Bar chart type
         data: {
-          labels: data.labels,
+          labels: ['Total Population'],
           datasets: [
             {
-              label: 'Civil Status as of 2020',
-              data: data.values,
-              backgroundColor: 'rgba(41, 155, 191)',  // Corrected color format
-              borderColor: 'rgba(41, 155, 191)',      // Corrected color format
+              label: 'Population as of 2020',
+              data: [population2020],
+              backgroundColor: 'rgba(41, 155, 191)',
+              borderColor: 'rgba(41, 155, 191)',
               borderWidth: 1
             },
             {
-              label: 'Civil Status as of 2015', // New dataset label
-              data: [34, 23, 43, 34], // New dataset values
+              label: 'Population as of 2015',
+              data: [population2015],
               backgroundColor: 'rgba(255, 0, 0)', // Red color
               borderColor: 'rgba(255, 0, 0)',     // Red color
               borderWidth: 1
@@ -28,17 +42,17 @@ document.addEventListener('DOMContentLoaded', function() {
         },
         options: {
           responsive: true,
-          tooltips: {
-            callbacks: {
-              label: function(tooltipItem, data) {
-                var dataset = data.datasets[tooltipItem.datasetIndex];
-                var total = dataset.data.reduce(function(previousValue, currentValue, currentIndex, array) {
-                  return previousValue + currentValue;
-                });
-                var currentValue = dataset.data[tooltipItem.index];
-                var percentage = ((currentValue / total) * 100).toFixed(2) + '%';
-                return data.labels[tooltipItem.index] + ': ' + percentage;
-              }
+          plugins: {
+            annotation: {
+              annotations: [
+                {
+                  type: 'text',
+                  position: 'center',
+                  x: '50%',
+                  y: '95%', // Adjust the y-position as needed
+                  content: 'Population ' + changeDescription + ' compared to 2015'
+                }
+              ]
             }
           }
         }
