6c2f4f3c847ee2f57632ae40bd5d2c56a9dc20e3
Glaisa Faye Adora <glaisafayeadora@gmail.com>
2023-09-12 17:04:26 +0800

    sd

--- dashboard/chart2.js
+++ dashboard/chart2.js
@@ -1,53 +1,46 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const ctx = document.getElementById('doughnut');
-
-  // Fetch data from the PHP script
-  fetch('/mis/dashboard/fetch_zone.php')
+    const ctx = document.getElementById('doughnut'); // Change 'lineChart' to 'barChart'
+  
+    // Fetch data from the PHP script
+    fetch('/mis/dashboard/fetch_household.php') // Correct the path to the PHP script
       .then(response => response.json())
       .then(data => {
-          new Chart(ctx, {
-              type: 'doughnut',
-              data: {
-                  labels: data.labels,
-                  datasets: [{
-                      label: 'By Zone',
-                      data: data.values,
-                      backgroundColor: [
-                          'rgba(41,155,991)',
-                          'rgba(400,162,235,1)',
-                          'rgba(255,206,86,1)',
-                          'rgba(120,46,139,1)',
-                          'rgba(300,100,800)'
-                      ],
-                      borderColor: [
-                          'rgba(41,155,991)',
-                          'rgba(400,162,235,1)',
-                          'rgba(255,206,86,1)',
-                          'rgba(120,46,139,1)',
-                          'rgba(300,100,800)'
-                      ],
-                      borderWidth: 1
-                  }]
+        new Chart(ctx, {
+          type: 'bar', // Bar chart type
+          data: {
+            labels: ['Total Population'],
+            datasets: [
+              {
+                label: 'Population as of 2020',
+                data: [data.totalPopulation], // Use the total population value
+                backgroundColor: 'rgba(41, 155, 191)',
+                borderColor: 'rgba(41, 155, 191)',
+                borderWidth: 1
               },
-              options: {
-                  responsive: true,
-                  tooltips: {
-                      callbacks: {
-                          label: function(tooltipItem, data) {
-                              var dataset = data.datasets[tooltipItem.datasetIndex];
-                              var total = dataset.data.reduce(function(previousValue, currentValue, currentIndex, array) {
-                                  return previousValue + currentValue;
-                              });
-                              var currentValue = dataset.data[tooltipItem.index];
-                              var percentage = ((currentValue / total) * 100).toFixed(2) + '%';
-                              return data.labels[tooltipItem.index] + ': ' + percentage;
-                          }
-                      }
-                  }                    
+              {
+                label: 'Population as of 2015',
+                data: [320], // Use the total population value
+                backgroundColor: 'rgba(255, 0, 0)', // Red color
+                borderColor: 'rgba(255, 0, 0)',     // Red color
+                borderWidth: 1
               }
-          });
+            ]
+          },
+          options: {
+            responsive: true,
+            tooltips: {
+              callbacks: {
+                label: function(tooltipItem, data) {
+                  var dataset = data.datasets[tooltipItem.datasetIndex];
+                  return dataset.label + ': ' + dataset.data[0]; // Display the total population value in the tooltip
+                }
+              }
+            }
+          }
+        });
       })
       .catch(error => {
-          console.error('Error fetching data:', error);
+        console.error('Error fetching data:', error);
       });
-});
+  });
+  
