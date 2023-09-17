cc04894bd9566842a321aca6d21171e610217d07
Glaisa Faye Adora <glaisafayeadora@gmail.com>
2023-09-17 19:41:25 +0800

    daot ang sidebar

--- certificates/certificates.php
+++ certificates/certificates.php
@@ -1,268 +1,136 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "mis";
+
+// Create connection
+$connection = new mysqli($servername, $username, $password, $database);
+
+$clearanceNo = "";
+$residentid = "";
+$purpose = "";
+$orNo = "";
+
+$errorMessage = "";
+$successMessage = "";
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    // Check if the POST data exists before accessing it
+    if (isset($_POST["clearanceNO"])) {
+        $clearanceNo = $_POST["clearanceNO"];
+    }
+    if (isset($_POST["ddl_resident"])) {
+        $residentid = $_POST["ddl_resident"];
+    }
+    if (isset($_POST["purpose"])) {
+        $purpose = $_POST["purpose"];
+    }
+    if (isset($_POST["orNo"])) {
+        $orNo = $_POST["orNo"];
+    }
+
+    if (empty($clearanceNo) || empty($residentid) || empty($purpose) || empty($orNo)) {
+        $errorMessage = "All fields are required";
+    } else {
+
+        $sql = "INSERT INTO clearance (`clearanceNO`, `residentid`, `purpose`, `orNo`) VALUES ('$clearanceNo', '$residentid', '$purpose', '$orNo')";
+        $result = $connection->query($sql);
+
+        if (!$result) {
+            $errorMessage = "Invalid query" . $connection->error;
+        } else {
+            $clearanceNo = "";
+            $residentid = "";
+            $purpose = "";
+            $orNo = "";
+
+            $successMessage = "Certificate added correctly";
+
+            header("location: /mis/certificates/certificates.php");
+            exit;
+        }
+    }
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles.css">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous"/>
     <title>Puntian Management Information System</title>
-
-    <style>
-    * {
-        padding: 0;
-        margin: 0;
-        box-sizing: border-box;
-        font-family: 'poppins', sans-serif;
-    }
-
-    .topbar {
-        position: fixed;
-        background: #fff;
-        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.08);
-        width: 100%;
-        height: 60px;
-        padding: 0 20px;
-        display: grid;
-        grid-template-columns: 2fr 10fr 0.4fr 1fr;
-        align-items: center;
-        z-index: 1;
-    }
-
-    .logo h2 {
-        color: #299b63;
-    }
-
-    .search {
-        position: relative;
-        width: 60%;
-        justify-self: center;
-    }
-
-    .search input {
-        width: 100%;
-        height: 40px;
-        padding: 0 40px;
-        font-size: 16px;
-        outline: none;
-        border: none;
-        border-radius: 10px;
-        background: #f5f5f5;
-    }
-
-    .search i {
-        position: absolute;
-        right: 15px;
-        top: 15px;
-        cursor: pointer;
-    }
-
-    .user {
-        position: relative;
-        width: 50px;
-        height: 50px;
-    }
-
-    .user img {
-        position: absolute;
-        top: 0;
-        left: 0;
-        height: 100%;
-        width: 100%;
-        object-fit: cover;
-    }
-
-    .sidebar {
-        position: fixed;
-        top: 60px;
-        width: 260px;
-        height: calc(100% - 60px);
-        background: #299b63;
-        overflow-x: hidden;
-    }
-
-    .sidebar ul {
-        margin-top: 20px;
-    }
-
-    .sidebar ul li {
-        width: 100%;
-        list-style: none;
-    }
-
-    .sidebar ul li a {
-        width: 100%;
-        text-decoration: none;
-        color: #fff;
-        height: 60px;
-        display: flex;
-        align-items: center;
-    }
-
-    .sidebar ul li i {
-        min-width: 60px;
-        font-size: 24px;
-        text-align: center;
-    }
-
-    .container{
-        display: flex;
-        flex-direction: column;
-    }
-
-    .main {
-        position: absolute;
-        top: 60px;
-        width: calc(100% - 260px);
-        left: 260px;
-        min-height: calc(100vh - 60px);
-        background: #f5f5f5;
-    }
-
-    .cards {
-        width: 100%;
-        padding: 35px 20px;
-        display: grid;
-        grid-template-columns: repeat(4, 1fr);
-        grid-gap: 20px;
-    }
-
-    .cards .card {
-        padding: 20px;
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-        background: #fff;
-        border-radius: 10px;
-        box-shadow: 0 7px 25px 0 rgba(0, 0, 0, 0.08);
-    }
-
-    .number {
-        font-size: 35px;
-        font-weight: 500;
-        color: #299b63;
-    }
-
-    .card-name {
-        color: #888;
-        font-weight: 600;
-    }
-
-    .icon-box i {
-        font-size: 45px;
-        color: #299b63;
-    }
-
-    .content {
-    padding: 20px;
-    }
-
-.btn-primary {
-    background-color: #299b63;
-    color: #fff;
-    padding: 10px 20px;
-    border: none;
-    border-radius: 5px;
-    text-decoration: none;
-    font-size: 16px;
-    cursor: pointer;
-}
-
-.btn-primary:hover {
-    background-color: #247b4d;
-}
-
-.table {
-    width: 100%;
-    border-collapse: collapse;
-    margin-top: 20px;
-    background-color: #fff;
-    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.08);
-}
-
-.table th, .table td {
-    padding: 10px 15px;
-    text-align: left;
-    border-bottom: 1px solid #e0e0e0;
-}
-
-.table th {
-    background-color: #f5f5f5;
-}
-
-.table tbody tr:hover {
-    background-color: #f5f5f5;
-}
-
-    .content-table{
-        border-collapse: collapse;
-        margin: 25px 0;
-        font-size: 16px;
-        min-width: 400px;
-    }
-
-.content-table thead tr {
-    background-color: #eeeee4;
-    color: #24ad50;
-    text-align: left;
-}
-
-.content-table th,
-.content-table td{
-    padding: 12px 15px;
-
-}
-</style>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
 
 </head>
 
 <body>
-    <div class="container">
-        <div class="topbar">
-            <div class="logo">
-                <h2>Puntian, Sumilao</h2>
-            </div>
-            <div class="search">
-                <input type="text" id="search" placeholder="search here">
-                <label for="search"> <i class="fas fa-search"></i></label>
-            </div>
-            <i class="fas fa-bell"></i>
-            <div class="user">
-                <img src="/mis/dashboard/logo.jpg" alt="">
-            </div>
+<?php
+    include 'layout.php';
+        ?>
+<div id="pop-up-modal" class="modal">
+        <div class="modal-content">
+        <h1>Select a Document Request</h1>
+            <form method="post" action="">
+                <select name="document_type" id="document_type">
+                    <option value="barangay_certificate">Barangay Clearance</option>
+                    <option value="business_clearance">Business Clearance</option>
+                    <option value="barangay_indigency">Barangay Indigency</option>
+                </select>
+                <button type="button" class="btn btn-secondary" id="close-modal">&times; Close</button>
+                <button type="button" class="btn btn-primary" id="nextButton">Next</button>
+            </form>
         </div>
-        <div class="sidebar">
-            <ul>
-                <?php
-                $menuItems = array(
-                    array("Dashboard", "fas fa-th-large", "/mis/dashboard/dashboard.php"),
-                    array("Officials", "fas fa-user-tie", "/mis/officials/officials.php"),
-                    array("Household Profiling","fas fa-house-user", "/mis/household/household.php"),
-                    array("Resident Profiling", "fas fa-users", "/mis/residents/residents.php"),
-                    array("Certification", "fas fa-print", "/mis/certificates/certificates.php"),
-                    array("Decision Support System", "fas fa-chart-bar", "/mis/dss/dss.php"),
-                );
+    </div>
 
-                foreach ($menuItems as $item) {
-                    echo '<li>';
-                    echo '<a href="' . $item[2] . '">'; 
-                    echo '<i class="' . $item[1] . '"></i>';
-                    echo '<div>' . $item[0] . '</div>';
-                    echo '</a>';
-                    echo '</li>';
-                }
-                ?>
-            </ul>
+    <div id="second-pop-up-modal" class="modal">
+        <div class="modal-content">
+        <h1>Enter Clearance Information</h1>
+            <form method="post" action="">
+                <input type="hidden" id="selected_document_type" name="selected_document_type">
+                <div class="row">
+                    <label for="clearanceNO">Clearance No:</label>
+                        <input type="text" class="form-control" id="clearanceNO" name="clearanceNO" value="<?php echo $clearanceNo; ?>">
+                </div>
+                <div class="row">
+                    <label for="ddl_resident">Resident:</label>
+                        <select id="ddl_resident" name="ddl_resident" class="form-control">
+                            <option value="" selected disabled>-- Select Resident --</option>
+                            <?php
+                            $squery = $connection->query("SELECT r.id, r.lname, r.fname FROM residents r");
+                            while ($row = $squery->fetch_assoc()) {
+                            echo '<option value="' . $row['id'] . '">' .$row['lname'].' , ' .$row['fname'] . '</option>';
+                            }
+                            ?>
+                        </select>
+                </div>
+                <div class="row">
+                    <label for="purpose">Purpose:</label>
+                        <input type="text" class="form-control" id="purpose" name="purpose" value="<?php echo $purpose; ?>">
+                </div>
+                <div class="row">
+                    <label for="orNo">OR Number:</label>
+                        <input type="text" class="form-control" id="orNo" name="orNo" value="<?php echo $orNo; ?>">
+                </div>
+                <div class="col-sm-3 d-grid">
+                    <a class="btn btn-outline-primary" href="/mis/certificates/certificates.php" role="button">Cancel</a>
+                </div>
+                <button type="submit" class="btn btn-primary" id="submitDocumentRequest">Submit Request</button>
+            </form>
         </div>
+    </div>
 
-        <div class="main">
-    <div class="content">
-        <div class="container my-5">
-            
-            <a class="btn btn-primary" href="/mis/certificates/certification.php" role="button">Add</a>
-            <br>
-            <table class="content-table">
+    
+
+    <div class="main">
+        <div class="content">
+            <div class="container my-5">
+                <div class="d-flex justify-content-center mb-3">
+                    <button type="button" class="btn btn-primary" id="showModal">Add Certificate</button>
+                </div>
+                <br>
+                <table class="content-table">
                 <thead>
                     <tr>
                         <th>Clearance #</th>
@@ -304,18 +172,51 @@
                     <td>{$row['purpose']}</td>
                     <td>{$row['orNo']}</td>
                     <td>
-                    <a class='btn btn-primary btn-sm' href='/mis/brgyclear/barclear.php'>Generate</a> 
+                    <a class='btn btn-primary btn-sm' href='/mis/certificates/brgyin.php'>Generate</a> 
                     </td>
                 </tr>
                 ";
-                 
                 }  
-                
-               ?>     
-               
+               ?>                
             </tbody> 
-        </table>  
-               
+        </table>           
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+    <script>
+        // Show the first modal
+        document.getElementById("showModal").addEventListener("click", function () {
+            var firstModal = document.getElementById("pop-up-modal");
+            firstModal.style.display = "block"; // Show the first modal
+        });
+
+        // Close the first modal when the close button is clicked
+        document.getElementById("close-modal").addEventListener("click", function () {
+            var firstModal = document.getElementById("pop-up-modal");
+            firstModal.style.display = "none"; // Hide the first modal
+        });
+
+        // Show the second modal when the "Next" button is clicked on the first modal
+        document.getElementById("nextButton").addEventListener("click", function () {
+            var selectedDocumentType = document.getElementById("document_type").value;
+            document.getElementById("selected_document_type").value = selectedDocumentType;
+
+            var firstModal = document.getElementById("pop-up-modal");
+            firstModal.style.display = "none"; // Hide the first modal
+
+            var secondModal = document.getElementById("second-pop-up-modal");
+            secondModal.style.display = "block"; // Show the second modal
+        });
+
+        // Close the second modal when the close button is clicked
+        document.getElementById("close-second-modal").addEventListener("click", function () {
+            var secondModal = document.getElementById("second-pop-up-modal");
+            secondModal.style.display = "none"; // Hide the second modal
+        });
+    </script>
+
+</body>
+
+</html>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
